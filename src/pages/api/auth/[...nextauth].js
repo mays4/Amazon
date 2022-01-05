e1d35aff5a6c950cc63e738c39a7c9de143a295f
mays4 <maysalani84@yahoo.com>
2022-01-04 19:32:19 -0500

    fixing issues

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -7,13 +7,7 @@ export default NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      authorization: {
-        params: {
-          prompt: "consent",
-          access_type: "offline",
-          response_type: "code"
-        }
-      }
+      
    
     }),
     // ...add more providers here
