cf520c26ce2214aeb0a5e25dc590c4a6e2ac91f9
mays4 <maysalani84@yahoo.com>
2022-01-07 19:15:59 -0500

    adding more stuff

--- next.config.js
+++ next.config.js
@@ -3,6 +3,6 @@ module.exports = {
     domains: ['links.papareact.com','fakestoreapi.com'],
   },
   env:{
-    stripe_pablic_key : process.env.STRIPE_PUBLIC_KEY
+    stripe_pablic_key : process.env.STRIPE_PUBLIC_KEY.toString()
   }
 };
